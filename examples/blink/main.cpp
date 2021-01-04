976a4800e69c535e1c5ad0b5ea57589575d4cfbf
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-01-04 16:19:30 +0100

    removed unnecessary dependencies and macros, added pinout mux definition

--- examples/blink/main.cpp
+++ examples/blink/main.cpp
@@ -1,12 +1,11 @@
 #include <util/delay.h>
 #include <avr/io.h>
-#include <string.h>
 #include "uart.h"
-
-#define USART0_BAUD_RATE(BAUD_RATE) ((float)(10000000 * 64 / (16 * (float)BAUD_RATE)) + 0.5)
 
 int main(void) {
   _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, CLKCTRL_PDIV_2X_gc | CLKCTRL_PEN_bm); // set prescaler to 2 -> 10MHz
+  PORTMUX.CTRLB = PORTMUX_USART0_ALTERNATE_gc || PORTMUX_SPI0_ALTERNATE_gc; // must be set at once, or the mcu might block
+
   DINIT();
 
   PORTB.DIRSET = PIN5_bm; // set PB5 (led) as output
