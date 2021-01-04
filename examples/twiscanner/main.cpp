a1389e648a6566c44d2ba2551b8fe249ae725779
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-01-04 16:43:41 +0100

    show temp/humidity as 'float' values

--- examples/twiscanner/main.cpp
+++ examples/twiscanner/main.cpp
@@ -14,23 +14,26 @@ int main(void) {
   uint8_t data[3] = {0};
 
   // read raw data from an SHT20
+  // see https://github.com/DFRobot/DFRobot_SHT20/blob/master/DFRobot_SHT20.cpp
   uint8_t status = twi_read_bytes(0x40, data, 0xe3, 2); // read temp hold
   if (status == 0) {
     uint32_t raw = ((uint32_t)data[0]<<8) | (uint32_t)data[1];
+    char out[6];
     // DF("%u %u\n", data[0], data[1]);
     // DF("temp raw: %lu\n", raw_temp);
     raw *= 17572;
     raw /= 65536;
     raw -= 4685;
-    DF("temp (precision 0.01): %lu\n", raw);
+    uart_u2c(out, raw, 2);
+    DF("temp: %sC\n", out);
 
     twi_read_bytes(0x40, data, 0xe5, 2); // read humidity hold
     raw = ((uint32_t)data[0]<<8) | (uint32_t)data[1];
     raw *= 12500;
     raw /= 65536;
     raw -= 6;
-    DF("humidity (precision 0.01): %lu\n", raw);
-
+    uart_u2c(out, raw, 2);
+    DF("humidity: %s%%\n", out);
   } else {
     DF("error: %u\n", status);
   }
