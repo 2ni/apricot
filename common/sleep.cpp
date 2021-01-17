385eb10e40a2dce484d91577e22de49c3b1e7b4d
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-01-17 12:59:26 +0100

    fix sleep_ms to be able to sleep longer than 2s, unfortunately with a less precise timing though. use _sleep() for precise (fixed) sleep times

--- common/sleep.cpp
+++ common/sleep.cpp
@@ -5,26 +5,26 @@
  * RTC.PER: 16bit max
  * using 32768Hz external crystal
  *
- * max sleep = 2^16*1000*prescaler/32768
- * min sleep = 1*1000*prescaler/32768
- * count = <delay in ms>*32768/1000/prescaler
+ * max sleep ms = 2^16*1000*p2^rescaler/32768
+ * min sleep ms = 1*1000*2^prescaler/32768
+ * count = <delay in ms>*32768/1000/2^prescaler
  *
- * -> 0.03ms  - 2s       (prescaler 1)     00:00:00:02
- *    0.06ms  - 4s       (prescaler 2)     00:00:00:04
- *    0.12ms  - 8s       (prescaler 4)     00:00:00:08
- *    0.24ms  - 16s      (prescaler 8)     00:00:00:16
- *    0.49ms  - 32s      (prescaler 16)    00:00:00:32
- *    0.98ms  - 64s      (prescaler 32)    00:00:01:04
- *    1.95ms  - 128s     (prescaler 64)    00:00:02:08
- *    3.91ms  - 256s     (prescaler 128)   00:00:04:16
- *    7.81ms  - 512s     (prescaler 256)   00:00:08:32
- *    15.625ms- 1024s    (prescaler 512)   00:00:17:04
- * -> 31.25ms - 2048s    (prescaler 1024)  00:00:34:08
- *    62.5ms  - 4096s    (prescaler 2048)  00:01:08:16
- *    125ms   - 8192s    (prescaler 4096)  00:02:16:32
- *    250ms   - 16384s   (prescaler 8192)  00:04:33:04
- *    500ms   - 32768s   (prescaler 16384) 00:09:06:08
- *    1s      - 65536s   (prescaler 32768) 00:18:12:16
+ * -> 0.03ms  - 2s       (prescaler 0  -> 1)     00:00:00:02
+ *    0.06ms  - 4s       (prescaler 1  -> 2)     00:00:00:04
+ *    0.12ms  - 8s       (prescaler 2  -> 4)     00:00:00:08
+ *    0.24ms  - 16s      (prescaler 3  -> 8)     00:00:00:16
+ *    0.49ms  - 32s      (prescaler 4  -> 16)    00:00:00:32
+ *    0.98ms  - 64s      (prescaler 5  -> 32)    00:00:01:04
+ *    1.95ms  - 128s     (prescaler 6  -> 64)    00:00:02:08
+ *    3.91ms  - 256s     (prescaler 7  -> 128)   00:00:04:16
+ *    7.81ms  - 512s     (prescaler 8  -> 256)   00:00:08:32
+ *    15.625ms- 1024s    (prescaler 9  -> 512)   00:00:17:04
+ * -> 31.25ms - 2048s    (prescaler 10 -> 1024)  00:00:34:08
+ *    62.5ms  - 4096s    (prescaler 11 -> 2048)  00:01:08:16
+ *    125ms   - 8192s    (prescaler 12 -> 4096)  00:02:16:32
+ *    250ms   - 16384s   (prescaler 13 -> 8192)  00:04:33:04
+ *    500ms   - 32768s   (prescaler 14 -> 16384) 00:09:06:08
+ *    1s      - 65536s   (prescaler 15 -> 32768) 00:18:12:16
  */
 
 ISR(RTC_CNT_vect) {
@@ -32,13 +32,11 @@ ISR(RTC_CNT_vect) {
 }
 
 /*
- * prescaler
- * 0 -> :1
- * 1 -> :2
- * 2 -> :4
- * 3 -> :8
- * ...
- * 9 -> :512
+ * ms = per*1000*2^prescaler/32768
+ * precision = 1000*2^prescaler/32768
+ * max: 2^16 * precision
+ *
+ * eg 5000ms: prescaler=5 (32), per=5120 -> precision=0.9765625
  */
 void _sleep(uint16_t per, uint8_t prescaler) {
   while (RTC.STATUS > 0);             // wait for all register to be synchronized
@@ -62,17 +60,17 @@ void _sleep(uint16_t per, uint8_t prescaler) {
 }
 
 /*
- * prescaler 1
+ * division is slow, avoid it! if you need speed, use _sleep()
+ * and precalculate "per" value
  *
- * 1ms - 64'000ms
- * (precision: 1000/1024 = 0.9765625 per bit)
- * max 2^16bit
+ * per = ms*32768/(1000*2^prescaler)
  *
- * division is slow, avoid it! if you need speed, use _sleep() and precalculate per value
+ * prescaler 2^5: 0.98ms - 64s
  *
  */
 void sleep_ms(uint16_t ms) {
-  _sleep((uint32_t)ms*33, 0); // 32768/1000 = 33 -> 1.007080078ms
+  _sleep(((uint32_t)ms*1024)/1000, 5); // 1024=32768/2^5, slow division!
+  // _sleep((uint32_t)ms*33, 0); // 32768/1000 = 33 -> 1.007080078ms
 }
 
 /* prescaler 1024
@@ -81,5 +79,5 @@ void sleep_ms(uint16_t ms) {
  * max 2^16bit
  */
 void sleep_s(uint16_t seconds) {
-  _sleep(seconds, 0xf); // prescaler 2^15 = 32768
+  _sleep(seconds, 15); // prescaler 2^15 = 32768
 }
