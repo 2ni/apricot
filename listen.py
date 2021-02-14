76cd3ae99b5f82f98275a618b1e53d508fd791f5
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-02-14 17:02:07 +0100

    -added some more output when receiving data (eg number of gateways seen, rssi of my gateway, numer of gateways with gps data, datarate, frequency - added a json example

--- listen.py
+++ listen.py
@@ -7,6 +7,7 @@ import json
 from dateutil.parser import parse
 from datetime import timezone
 import sys
+import re
 
 try:
     from credentials import appid, key
@@ -27,11 +28,31 @@ def on_subscribe(mosq, obj, mid, granted_qos):
 def on_message(mqttc, obj, msg):
 
     try:
-        x = json.loads(msg.payload.decode('utf-8'))
-        timestamp = parse(x['metadata']['time']).replace(tzinfo=timezone.utc).astimezone(tz=None)
+        x = json.loads(msg.payload.decode("utf-8"))
+        devid = x["dev_id"]
+        counter = x["counter"]
+        gateways = x["metadata"]["gateways"]
+        gws_data = [(x['rssi'], x['latitude'], x['longitude']) for x in gateways if 'latitude' in x]
+        timestamp = parse(x["metadata"]["time"]).replace(tzinfo=timezone.utc).astimezone(tz=None)
         timestamp_str = timestamp.strftime("%Y-%m-%d %H:%M:%S")
-        alt_data = str("%s" % x['payload_fields'])
-        print(timestamp_str, alt_data)
+        data = str("%s" % x["payload_fields"])
+        my_rssi = 0
+        for gw in gateways:
+            if gw["gtw_id"] == "eui-58a0cbfffe01bc78":
+                my_rssi = gw["rssi"]
+
+        print("{timestamp}: ({devid}:{counter},{datarate}|{frq}|{my_rssi}) {data} | gws ({no_gws_pos}/{no_gws}): {gws_data}".format(
+            timestamp=timestamp_str,
+            devid=devid,
+            counter=counter,
+            datarate=re.findall("^(.*)BW", x["metadata"]["data_rate"])[0],
+            frq=x["metadata"]["frequency"],
+            data=data,
+            no_gws=len(gateways),
+            no_gws_pos=len(gws_data),
+            gws_data=gws_data,
+            my_rssi=my_rssi
+        ))
 
     except Exception as e:
         print(e)
