f4bcb0f1dc6321172c1ddf90ac9bc837d772458a
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-02-22 08:05:26 +0100

    fix sleep duration, which was changed with the introduction of apricot. So far the loop was almos not sleeping at all

--- examples/touch/main.cpp
+++ examples/touch/main.cpp
@@ -1,3 +1,9 @@
+/*
+ * example of capacitive touch with optimized
+ * sleep mode
+ * every 50ms the touch is checking for pressing
+ * the measurement lasts ~250us
+ */
 #include <avr/io.h>
 #include "uart.h"
 #include "mcu.h"
@@ -6,10 +12,11 @@
 #include "sleep.h"
 #include "millis.h"
 
-
+// to avoid complex division
+// we calculate the needed timer count for the sleep function beforehand
+// and use the internal function _s_sleep()
 #define SLEEP_TIME 50
-// to avoid complex multiplications we calculate the needed timer count beforehand and use the internal function _s_sleep()
-#define SLEEP_PER UINT32_C(SLEEP_TIME)*32768/1000
+#define SLEEP_PER UINT32_C(SLEEP_TIME)*1024/1000
 
 int main(void) {
   mcu_init();
@@ -17,15 +24,22 @@ int main(void) {
   TOUCH button(&PB7);
   // TOUCH button(&PA3, 50, 100); // alu strip to check if someone is sitting on a chair
 
+  pins_disable_buffer();
+
   uint8_t is_pressed;
 
   while (1) {
+    pins_enable_buffer(&pins_led);
+    pins_enable_buffer(&PB7);
     is_pressed = button.is_pressed(&pins_led);
+    pins_disable_buffer(&PB7);
     if (is_pressed) {
       // DF("pressed: 0x%02x\n", is_pressed & ~touch_is_pressed_bm);
       // set led when long touch was reached
       if (is_pressed & touch_long_bm) {
         pins_set(&pins_led, 1);
+      } else if (is_pressed & touch_verylong_bm) {
+        pins_set(&pins_led, 0);
       }
     }
 
@@ -35,9 +49,11 @@ int main(void) {
       DF("released: 0x%02x\n", is_pressed);
     }
 
-    // do not sleep if pressed, as we need to measure time (whic is on hold while sleeping)
+    // do not sleep if pressed, as we need to measure time for short/long touch (which is on hold if sleeping)
     if (!is_pressed) {
-      _sleep(SLEEP_PER, 0); // 1638 (precalculated) vs 1650 using sleep_ms(50);
+      pins_disable_buffer(&pins_led);
+      _sleep(SLEEP_PER, 5); // 1638 (precalculated) vs 1650 using sleep_ms(50);
+      pins_enable_buffer(&pins_led);
     }
   }
 }
