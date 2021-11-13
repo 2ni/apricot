43c6917b69d1a19648b99339147c07a8d7711d30
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-11-13 13:49:50 +0100

    -support rssi limit

--- examples/rfm69/main.cpp
+++ examples/rfm69/main.cpp
@@ -16,9 +16,12 @@ int main(void) {
   RFM69 rfm69;
   RFM69::Packet response;
   uint8_t counter = 0;
-  uint8_t len = 5;
-  char msg[len];
+  uint8_t len;
+  char msg[8];
   uint16_t vcc;
+  uint8_t rssi_limit_reached = 0;
+  int8_t pwrchange;
+  uint8_t last_rssi;
 
   uint8_t version = rfm69.init(get_deviceid(), NETWORK);
   if (!version) {
@@ -28,18 +31,29 @@ int main(void) {
   }
 
 
+  rfm69.set_power_level(10);
   while (1) {
     vcc = get_vin();
+    len = 5;
     msg[0] = (0x00<<4) | 0x01; // type: dbg (0x00), 1byte
     msg[1] = counter;
     msg[2] = (0x01<<4) | 0x02; // type: vcc (0x01), 2bytes
     msg[3] = vcc >> 8;
     msg[4] = vcc & 0xff;
 
+    if (rssi_limit_reached) {
+      rssi_limit_reached = 0;
+      msg[5] = (0x03<<4) | 0x02; // type: rssi (0x03), 2bytes
+      msg[6] = 0x80;
+      msg[7] = last_rssi;
+      len += 3;
+    }
+
     DF("sending %03u | ", counter);
     if (rfm69.send_retry(GATEWAY, msg, len, &response, 2)) {
+      last_rssi = (uint8_t)(-response.rssi);
       // DF(OK("from 0x%06lX: '%s' (%idBm)") "\n", response.from, response.payload, response.rssi);
-      D("ack | ");
+      DF("ack (%idBm) | ", response.rssi);
       uart_arr("raw response", response.payload, response.len, 0);
       D(" | ");
       uint8_t i = 0;
@@ -57,19 +71,18 @@ int main(void) {
               | ((uint16_t)response.payload[i+2] << 8)
               | response.payload[i+3];
 
-            i += data_len;
             DF(OK("timestamp: %lu") " | ", timestamp);
             break;
-          case 0x03: // case rssi
-            DF("pwr change: %i | ", (int8_t)response.payload[i]);
-            rfm69.set_power_level_relative(response.payload[i]);
-            DF("new pwr: %u | ", rfm69.get_power_level());
-            i += data_len;
+          case 0x03: // type: rssi (0x03), 1 byte
+            pwrchange = response.payload[i] & 0x0f | (response.payload[i] & 0x08 ? 0xf0 : 0x00); // convert int4_t to int8_t
+            rssi_limit_reached = !rfm69.set_power_level_relative(pwrchange);
+            DF("pwr: %i -> %u  (%u) | ", pwrchange, rfm69.get_power_level(), rssi_limit_reached);
             break;
           default:
             DF(NOK("unknown type 0x%02x") " | ", data_type);
             break;
         }
+        i += data_len;
       }
       DL("");
     } else {
