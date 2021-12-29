8356e0418a3501e1a9449d633c3a15a82b88c823
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2021-12-29 13:27:02 +0100

    create simple dcc accessory packet

--- examples/dcc/README.md
+++ examples/dcc/README.md
@@ -4,6 +4,9 @@ h4. Links
 - https://www.opendcc.de/elektronik/opendcc/opendcc_sw_lenz.html
 - https://dccwiki.com/Digital_packet
 - https://dccwiki.com/DCC_Tutorial_(Power)
+- http://normen.railcommunity.de/RCN-211.pdf
+- https://github.com/mrrwa/NmraDcc/blob/master/NmraDcc.cpp
+- https://www.opendcc.de/elektronik/opendecoder/OpenDecoder_V04.zip
 
 
 h4. Knowledge
--- examples/dcc/main.cpp
+++ examples/dcc/main.cpp
@@ -1,3 +1,7 @@
+/*
+ * creates a DCC signal (simulator)
+ */
+
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
@@ -6,6 +10,25 @@
 #include "pins.h"
 
 volatile uint8_t done = 0;
+volatile uint8_t trackpos = 0;
+
+/*
+ * isr for uart read
+ */
+ISR(USART0_RXC_vect) {
+  uint8_t in = USART0.RXDATAL;
+  switch (in) {
+    case '1':
+      trackpos = 1;
+      break;
+    case '2':
+      trackpos = 2;
+      break;
+    case '\n':
+      pins_flash(&pins_led, 1, 100);
+      break;
+  }
+}
 
 ISR(TCA0_OVF_vect) {
   done = 1;
@@ -20,36 +43,66 @@ void timer_init() {
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm;
 }
 
-void send_bit(uint8_t length) {
-  PORTA.OUTSET = PIN7_bm;
+void send_bit(uint8_t length, pins_t *pin) {
+  pins_set(pin, 1);
+  TCA0.SINGLE.CNT = 0;
   TCA0.SINGLE.PER = length ? 580 : 1160;
   done = 0;
-  while (!done);
-  PORTA.OUTCLR = PIN7_bm;
+  while (!done); // wait for interrupt
+  pins_set(pin, 0);
   done = 0;
-  while (!done);
+  while (!done); // wait for interrupt
 }
 
-void send_byte(uint8_t data) {
+void send_byte(uint8_t data, pins_t *pin) {
   for (uint8_t c=0; c<8; c++) {
-    send_bit(data & (1<<(7-c)));
+    send_bit(data & (1<<(7-c)), pin);
   }
 }
 
-void send_packet(uint8_t addr, uint8_t data) {
+/*
+ * length: 3-6bytes (inc last xor byte)
+ * turnout: 9bit address (accessory decoder 9bit address)
+ * signal: 11bit address (accessory decoder extended 14bit address)
+ *
+ * https://dccwiki.com/Address_Range
+ *
+ * https://www.opendcc.de/elektronik/opendcc/opendcc_sw_lenz.html
+ * addr 0      : broadcast for locos
+ * addr 1-127  : addr starts with 0 -> loco cmd with short addr
+ * addr 128-191: accessory decoders
+ * addr 192-231: loco long addr, following addrbits in next byte
+ *
+ * example loco short addr:
+ * addr loco: 0AAAAAAA -> realaddr & 0x7F
+ * data loco: 01DCSSSS -> 0x40 | (speed & 0x0F) | (direction << 5)
+ *            C   : light
+ *            SSSS: speed 0-14
+ *            D   : direction
+ *
+ * example track (aka Weiche):
+ * addr track: 10AAAAAA -> 0x80 + (realaddr & 0x3F)
+ * data track: 1AAA1BBR -> 0x80 + (realaddr / 0x40) ^ 0x07) * 0x10
+ *             A8-A6: are inverted
+ *             BB   : local addr on the decoder (0, 1, 2, 3)
+ *             R    : output bit
+ *
+ * we only support 3bytes (addr, data, xor)
+ */
+void send_packet(uint8_t addr, uint8_t data, pins_t *pin) {
   uint8_t c = 0;
   // preamble
   while (c<12) {
-    send_bit(1);
+    send_bit(1, pin);
     c++;
   }
-  send_bit(0);
-  send_byte(addr);
-  send_bit(0);
-  send_byte(data);
-  send_bit(0);
-  send_byte(addr ^ data);
-  send_bit(1);
+  send_bit(0, pin);
+  send_byte(addr, pin);
+  send_bit(0, pin);
+  send_byte(data, pin);
+  send_bit(0, pin);
+  send_byte(addr ^ data, pin);
+  send_bit(1, pin);
   // DF("a: 0x%02x, d: 0x%02x, c: 0x%02x\n", addr, data, addr^data);
 }
 
@@ -74,59 +127,43 @@ void send_packet(uint8_t addr, uint8_t data) {
  * BB: which port
  * R: which couple of the port (ports usually come in couples)
  */
-void track_switch(uint16_t addr, uint8_t local_addr, uint8_t output) {
-  uint8_t a = 0x80 + (addr & 0x3f);
+void track_switch(uint16_t addr, uint8_t local_addr, uint8_t output, pins_t *pin) {
+  uint8_t a = 0x80 + (addr & 0x3f); // a 128-191: 10AAAAAA, 1AAA1BBR (sometimes: 11AAAAAA, 1AAACDDD)
   //                 ----------- AAA -----------   - C -    ---------- BB ----------   ------ R ------
-  uint8_t d = 0x80 + (((addr >> 6) ^ 0x07) << 4) + (1<<3) + ((local_addr & 0x03)<<1) + (output & 0x00);
-  send_packet(a, d);
+  uint8_t d = 0x80 + ((~addr & 0x1c0)>>2) + (1<<3) + ((local_addr & 0x03)<<1) + (output & 0x01);
+  // DF("addr: 0x%03x, local: 0x%02x, a: 0x%02x, d: 0x%02x\n", addr, local_addr, a, d);
+  send_packet(a, d, pin); // in  oscilloscope 9-bit-address is shown as msb!
+  // control (see https://github.com/mrrwa/NmraDcc/blob/master/NmraDcc.cpp +1339)
+  // int16_t ba = (((~d) & 0b01110000)<<2) | (a & 0b00111111);
+  // DF("ba: %i\n", ba);
 }
 
 int main(void) {
-  mcu_init();
+  mcu_init(1);
 
   pins_t L = PB7;
   pins_t R = PB6;
+  pins_t SIGNAL = PB6; // control signal
 
   pins_output(&L, 1);
   pins_output(&R, 1);
-
-  uint8_t out = 0;
-  uint8_t led_count = 0;
-  uint8_t delay = 50;
-  while (1) {
-    pins_set(&L, out);
-    pins_set(&R, !out);
-
-    out = !out;
-    if (led_count == (500/delay)) {
-      DL("blink");
-      pins_toggle(&pins_led);
-      led_count = 0;
-    } else {
-      led_count++;
-    }
-
-    _delay_ms(delay);
-  }
-
-  // TODO replace with R, L: PORTA.DIRSET = PIN7_bm;
+  pins_output(&SIGNAL, 1);
 
   timer_init();
 
-  track_switch(0x01, 2, 1);
-  send_bit(0);
-  while(1);
-  send_packet(0xff, 0x00); // idle packet
+  DL("usage:\n 1 = track position 0\n 2 = track position 1");
 
-  uint8_t data = 0xf1;
-
-  for (uint8_t c=0; c<8; c++) {
-      PORTA.OUTSET = PIN7_bm;
-      TCA0.SINGLE.PER = data & (1<<(7-c)) ? 580 : 1160;
-      done = 0;
-      while (!done);
-      PORTA.OUTCLR = PIN7_bm;
-      done = 0;
-      while (!done);
+  while  (1) {
+    // 1 or 2 from keyboard
+    if (trackpos) {
+      track_switch(0x003, 1, trackpos-1, &SIGNAL); // trackpos is 1(=off) or 2(=on)
+      DF("trackpos: %u sent\n", trackpos-1);
+      trackpos = 0;
+    } else {
+      send_packet(0xff, 0x00, &SIGNAL); // idle packet: preamble 0 11111111 0 00000000 0 11111111 1
+      DL("idle packet");
+    }
+    send_bit(0, &SIGNAL);
+    _delay_ms(2000);
   }
 }
