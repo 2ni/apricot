41b000ba2a27cd9c944e73736c9af2ffa9652c51
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2022-11-01 11:43:44 +0100

    added example to create pwm with tcd at 78kHz

--- examples/pwm/main.cpp
+++ examples/pwm/main.cpp
@@ -24,16 +24,38 @@
  * TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_WGMODE_FRQ_gc;
  *
  * PWM WITH DUTY CYCLE 38kHz 1/4 (TCA0)
+ * only works in idle sleep mode
  * TCA0.SINGLE.PER = 263; // frequency=10MHz/(PER+1), use PERBUF to dynamically change w/o glitches
  * TCA0.SINGLE.CMP1 = 66; // duty cycly (max PER), user CMP1BUF to dynamicall change w/o glitches
  * TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc | TCA_SINGLE_ENABLE_bm;
  * TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP1EN_bm | TCA_SINGLE_WGMODE_SINGLESLOPE_gc;
  *
  * PWM WITH DUTY CYCLE 38kHz 1/4 (TCB0/1, outputs: PA5 for TCB0, PA3 for TCB1
+ * works in idle, standby mode
  * TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc | TCB_ENABLE_bm; // max duration: 1/10MHz*2 * 2^16 = 13.1ms
  * TCB0.CTRLB = TCB_CCMPEN_bm | TCB_CNTMODE_PWM8_gc;
  * TCB0.CNT = 0;
  * TCB0.CCMP = (33<<8) | 132; // LSB: signal period, MSB: duty cycle (LSB+1)*dutycycle/100%
+ *
+ * PWM WITH DUTY CYCLE 76kHz (TCD, outputs: PA4, PA5) in two ramp mode
+ * only works in idle sleep mode
+ * PA5 will be inverted part of PA4 + deadtime
+ * see also examples/mppt
+ * uint8_t deadtime = 2;   // ms: deadtime/20MHz
+ * uint8_t duty = 25;      // 0-100
+ * uint16_t counter = 255; // frq = 20MHz/counter
+ * TCD0.CTRLA = TCD_CNTPRES_DIV1_gc | TCD_SYNCPRES_DIV1_gc | TCD_CLKSEL_20MHZ_gc;
+ * TCD0.CTRLB = TCD_WGMODE_TWORAMP_gc;
+ * _PROTECTED_WRITE(TCD0.FAULTCTRL, TCD_CMPAEN_bm | TCD_CMPBEN_bm);
+
+ * TCD0.CMPASET = deadtime;
+ * TCD0.CMPACLR = counter*duty/100;
+ * TCD0.CMPBSET = deadtime;
+ * TCD0.CMPBCLR = counter-TCD0.CMPACLR;
+
+ * while (!(TCD0.STATUS & TCD_ENRDY_bm)); // wait for any synch going on
+ * TCD0.CTRLE = TCD_SYNC_bm;
+ * TCD0.CTRLA |= TCD_ENABLE_bm;
  *
  */
 
